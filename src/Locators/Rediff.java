d369d6aee1b79259a217bdcf8f335cbe631a9297
Ganesh6842 <ganeshmalviya33@gmail.com>
2023-09-03 21:10:57 +0530

    ok

--- src/Locators/Rediff.java
+++ src/Locators/Rediff.java
@@ -3,18 +3,19 @@ package Locators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Rediff {
 
 	public static void main(String[] args) throws InterruptedException {
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver = new FirefoxDriver();
 	driver.get("http://register.rediff.com/register/register.php?FormName=user_details");
+	Thread.sleep(3000);
+	driver.findElement(By.tagName("input")).sendKeys("admin");
 	Thread.sleep(4000);
-	driver.findElement(By.tagName("input")).sendKeys("Ganesh Malviya");
+	driver.findElement(By.xpath(".//input[@id='newpasswd']")).sendKeys("admin@123");
 	Thread.sleep(4000);
-	driver.findElement(By.tagName("text")).sendKeys("ganeshmalviya");
-	Thread.sleep(4000);
-	driver.findElement(By.id("newpasswd")).sendKeys("chacha@123");
+	driver.findElement(By.id(".//input[@name='confirm_passwdc8630760']")).sendKeys("admin@123");
 	Thread.sleep(4000);
 	}
 
